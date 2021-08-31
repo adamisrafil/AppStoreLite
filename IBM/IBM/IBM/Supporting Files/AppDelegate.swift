6ce7fa04fd0ab04f8f6a20df50241ee89be44a80
adamscout162 <adam.israfil@gmail.com>
2021-08-31 12:53:20 -0400

    Initializing Project
    Removed all SwiftUI/SceneDelegate related files/code, creating folder for supporting files

---
+++ IBM/IBM/IBM/Supporting Files/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  IBM
+//
+//  Created by Adam Israfil on 8/31/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
