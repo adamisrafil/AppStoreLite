7e4d8681b1b996ca2ce737434471199a7a2bd20c
adamscout162 <adam.israfil@gmail.com>
2021-08-31 17:12:32 -0400

    Created/Updated basic ViewControllers

--- IBM/IBM/IBM/View Controllers/AppInfoViewController.swift
+++ IBM/IBM/IBM/View Controllers/AppInfoViewController.swift
@@ -1,19 +1,18 @@
 //
-//  ViewController.swift
+//  AppInfoViewController.swift
 //  IBM
 //
 //  Created by Adam Israfil on 8/31/21.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class AppInfoViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
 }
-
---
+++ IBM/IBM/IBM/View Controllers/AppSearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  IBM
+//
+//  Created by Adam Israfil on 8/31/21.
+//
+
+import UIKit
+
+class AppSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
